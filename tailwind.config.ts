e312b9aeda17eb31bbc3b112dfc9e0651c232f28
Diego Oliveira <diegooliveirabryto@gmail.com>
2023-12-14 13:53:01 -0300

    chore: initial config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,31 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from "tailwindcss";
 
 const config: Config = {
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      colors: {
+        black: "#767676",
+        gray: "#B1B1B1",
+        grayLight: "#ECECEC",
+        green: "#07AF9D",
+        red: "#FA4949",
+        yellow: "#FECE01",
+        white: "#F6F6F5",
+      },
+      spacing: {
+        "7xl": "85.5rem",
+        "8xl": "93.75rem",
+      },
+      maxWidth: {
+        "8xl": "87.5rem",
       },
     },
   },
   plugins: [],
-}
-export default config
+};
+export default config;
