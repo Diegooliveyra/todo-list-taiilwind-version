df6e9e31398665cc0adc964cdceffd50d598b547
Diego Oliveira <diegooliveirabryto@gmail.com>
2023-12-15 10:00:55 -0300

    feat: add hook useList

---
+++ src/hooks/useList.tsx
@@ -0,0 +1,59 @@
+import { TodoContext } from "@/provider/todo.provider";
+import { ITaskDTO } from "@/service/tasks.interface";
+import { getTask } from "@/service/tasks.service";
+import { IPagination, PaginationHelper } from "@/ultis/pagination-helper";
+import { StatusEnum } from "@prisma/client";
+import { useCallback, useContext, useEffect, useState } from "react";
+
+const UseList = (status?: StatusEnum) => {
+  const { setTodos } = useContext(TodoContext);
+  const [numberPage, setNumberPage] = useState<number>(1);
+  const [pagination, setPagination] = useState<IPagination>();
+  const [loading, setLoading] = useState<boolean>(false);
+  const [tasks, setTasks] = useState<ITaskDTO[]>([]);
+
+  useEffect(() => {
+    setNumberPage(1);
+  }, [status]);
+
+  const getTasksList = useCallback(
+    async (numberPage: number) => {
+      setLoading(true);
+
+      try {
+        let params = {};
+        if (status)
+          params = {
+            status,
+          };
+        const data = await getTask({
+          ...params,
+          page: numberPage,
+          size: 5,
+        });
+        setTasks(data.content);
+        setTodos(data.content);
+        setPagination(PaginationHelper.parserPagination<ITaskDTO>(data, setNumberPage));
+      } catch (error) {
+        console.error(error);
+      } finally {
+        setLoading(false);
+      }
+    },
+    [status],
+  );
+
+  useEffect(() => {
+    getTasksList(numberPage);
+  }, [getTasksList, numberPage]);
+
+  return {
+    numberPage,
+    setNumberPage,
+    pagination,
+    tasks,
+    loading,
+  };
+};
+
+export default UseList;
